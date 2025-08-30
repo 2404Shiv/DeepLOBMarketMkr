aff215b416e89c5037aa79501a609b05efd44bb0
2404Shiv <shivamshah1@hotmail.co.uk>
2025-08-30 22:49:24 +0100

    Create test_rf.R

---
+++ tests/testthat/test_rf.R
@@ -0,0 +1,12 @@
+test_that("RF scripts parse and rf_trades.csv is sane if present", {
+  expect_error(source(proj_path("R", "03_rf_backtest.R"), local = TRUE), NA)
+
+  p <- find_one("rf_trades.csv")
+  if (is.na(p)) testthat::skip("rf_trades.csv not found in repo/CI run")
+
+  dat <- read_if(p)
+  expect_true(is.data.frame(dat))
+  expect_true(nrow(dat) > 0)
+  expect_true(has_pnl(dat))
+  expect_true(has_ts(dat))
+})
