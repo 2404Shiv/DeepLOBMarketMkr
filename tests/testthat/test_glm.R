879a4a8eea946fa812f9f4d25b86ad788dd45952
2404Shiv <shivamshah1@hotmail.co.uk>
2025-08-31 14:29:02 +0100

    Update test_glm.R

--- tests/testthat/test_glm.R
+++ tests/testthat/test_glm.R
@@ -1,12 +1,12 @@
 test_that("GLM scripts are syntactically valid; glm_trades.csv sane if present", {
-  safe_parse(file.path(.root, "R", "03_glm_backtest.R"))
+  safe_parse(file.path("R", "03_glm_backtest.R"))
 
   p <- find_one("glm_trades.csv")
   if (is.na(p)) testthat::skip("glm_trades.csv not found in repo/CI run")
 
   dat <- read_if(p)
-  testthat::expect_true(is.data.frame(dat))
-  testthat::expect_true(nrow(dat) > 0)
-  testthat::expect_true(has_pnl(dat))
-  testthat::expect_true(has_ts(dat))
+  expect_true(is.data.frame(dat))
+  expect_true(nrow(dat) > 0)
+  expect_true(has_pnl(dat))
+  expect_true(has_ts(dat))
 })
