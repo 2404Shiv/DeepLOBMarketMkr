c46218e624205d3062780a5077e40ad693e15a4d
2404Shiv <shivamshah1@hotmail.co.uk>
2025-08-31 14:29:14 +0100

    Update test_rf.R

--- tests/testthat/test_rf.R
+++ tests/testthat/test_rf.R
@@ -1,12 +1,12 @@
 test_that("RF scripts are syntactically valid; rf_trades.csv sane if present", {
-  safe_parse(file.path(.root, "R", "03_rf_backtest.R"))
+  safe_parse(file.path("R", "03_rf_backtest.R"))
 
   p <- find_one("rf_trades.csv")
   if (is.na(p)) testthat::skip("rf_trades.csv not found in repo/CI run")
 
   dat <- read_if(p)
-  testthat::expect_true(is.data.frame(dat))
-  testthat::expect_true(nrow(dat) > 0)
-  testthat::expect_true(has_pnl(dat))
-  testthat::expect_true(has_ts(dat))
+  expect_true(is.data.frame(dat))
+  expect_true(nrow(dat) > 0)
+  expect_true(has_pnl(dat))
+  expect_true(has_ts(dat))
 })
