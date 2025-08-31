f9caa87f34dfbf86d48a0754c503c011c056d778
2404Shiv <shivamshah1@hotmail.co.uk>
2025-08-31 14:34:50 +0100

    Update test_rl.R

--- tests/testthat/test_rl.R
+++ tests/testthat/test_rl.R
@@ -1,10 +1,6 @@
-test_that("RL scripts are syntactically valid; rl_trades.csv sane if present", {
-  safe_parse(file.path("R", "04_train_RL_agent.R"))
-  safe_parse(file.path("R", "05_RL_backtest.R"))
-
+test_that("RL outputs look sane if present", {
   p <- find_one("rl_trades.csv")
-  if (is.na(p)) testthat::skip("rl_trades.csv not found in repo/CI run")
-
+  if (is.na(p)) testthat::skip("rl_trades.csv not found in repo/CI")
   dat <- read_if(p)
   expect_true(is.data.frame(dat))
   expect_true(nrow(dat) > 0)
